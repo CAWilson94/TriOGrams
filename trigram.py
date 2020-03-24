ad50131f2c93f7b8a9eb0f8a14da99fd89cc07bf
charlotte <charlotteawilson.94@gmail.com>
2020-03-24 22:47:25 +0000

    gen text basic - fix formatting for this

--- trigram.py
+++ trigram.py
@@ -26,7 +26,6 @@ class Trigram:
                 triDict[bigram] = [input_list[word+2]]
             else:
                 triDict[bigram].append(input_list[word+2])
-        pprint(triDict)
         return (triDict)
 
     def get_trigrams(self, line, word, triDict):
@@ -36,25 +35,23 @@ class Trigram:
             triDict[bigram] = [line[word+2]]
         else:
             triDict[bigram].append(line[word+2])
-        print(triDict)
         return triDict
 
 
     def text_generator(self): 
         """ probably want to use a generator for this... """ 
         new_text = []
-        text_dictionary = self.find_trigrams()
+        text_dictionary = self.get_trigrams_from_input()
         value_pair = random.choice(list(text_dictionary.keys()))
         new_text.append(value_pair)
         try: 
-            while(text_dictionary.get(value_pair, None)):             
-                new_text.append(text_dictionary[value_pair][-1])
-                value_pair = value_pair.split(" ")[-1] + " " + new_text[-1]
-            final_text = " ".join(new_text)
-            print("done")
-            print(final_text)
+            while(text_dictionary[value_pair] != "THE END"):     
+                if(value_pair in text_dictionary.keys()):        
+                    new_text.append(text_dictionary[value_pair][-1])
+                    value_pair = value_pair.split(" ")[-1] + " " + new_text[-1]
+                final_text = " ".join(new_text)
         except Exception as e: 
-            logging.warn("wit is going on: %s", e)
+            logging.warning("wit is going on: %s", e)
         return final_text.split()
 
     def text_generator_search_longest(self): 
@@ -70,7 +67,10 @@ def main():
     input_book = "book.txt"
     tri_o_gram = Trigram(input_book)
     #tri_o_gram.text_generator_search_longest()
-    tri_o_gram.get_trigrams_from_input()
+    generated_text = tri_o_gram.text_generator()
+    print(generated_text)
+    #print(generated_text)
+    #tri_o_gram.get_trigrams_from_input()
 
 if __name__ == '__main__':
     main()
